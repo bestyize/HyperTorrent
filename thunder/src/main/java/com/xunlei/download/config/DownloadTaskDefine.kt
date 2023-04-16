8a2b43bae9e2507ddcf8b16b6b1e6cf7ea84111e
bestyize <3011429848@qq.com>
2023-04-17 00:14:14 +0800

    优化权限管理
    Signed-off-by: bestyize <3011429848@qq.com>

--- thunder/src/main/java/com/xunlei/download/config/DownloadTaskDefine.kt
+++ thunder/src/main/java/com/xunlei/download/config/DownloadTaskDefine.kt
@@ -14,7 +14,7 @@ val TORRENT_PREFIX = "magnet:?xt=urn:btih:"
 
 val STORAGE_ROOT by lazy { Environment.getExternalStorageDirectory().absolutePath }
 
-val BASE_DIR by lazy { Environment.getExternalStorageDirectory().absolutePath + File.separator + "a.torrent" }
+val BASE_DIR by lazy { Environment.getExternalStorageDirectory().absolutePath + File.separator + "Download" + File.separator + "a.hyper"}
 val BASE_DOWNLOAD_DIR by lazy { BASE_DIR + File.separator + "download" + File.separator}
 val BASE_PICTURE_DOWNLOAD_DIR by lazy {
     val folder = BASE_DOWNLOAD_DIR + "picture" +File.separator
